492b819119856a3d31e45a4acdd8019584d7d100
naveenthakur1703 <naveenthakur7914@gmail.com>
2026-07-18 11:42:36 +0530

    Update sitemap with Concrete Calculator

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -14,5 +14,11 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: "monthly",
       priority: 0.9,
     },
+    {
+      url: "https://buildestic.com/concrete-calculator",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.9,
+    },
   ];
 }
